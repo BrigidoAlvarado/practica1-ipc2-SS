41c313519de0d958e84c04a405760215a46d5d0c
Brigido Alvarado <brigidoavarado@gmail.com>
2024-08-04 16:38:00 -0600

    crecion del proyecto en java maven

---
+++ CardManager/src/main/java/com/ipc2ss/cardmanager/CardManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ipc2ss.cardmanager;
+
+/**
+ *
+ * @author brigidoalvarado
+ */
+public class CardManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
