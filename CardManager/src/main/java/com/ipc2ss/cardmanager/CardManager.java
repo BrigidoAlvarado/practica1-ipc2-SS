1ad6fb1354cc205d1db56d8166af235e00544287
Brigido Alvarado <brigidoavarado@gmail.com>
2024-08-06 01:52:50 -0600

    implementacion del lector del tipo de solicitud y la validacion del formato de entrada

--- CardManager/src/main/java/com/ipc2ss/cardmanager/CardManager.java
+++ CardManager/src/main/java/com/ipc2ss/cardmanager/CardManager.java
@@ -4,6 +4,14 @@
 
 package com.ipc2ss.cardmanager;
 
+import com.ipc2ss.cardmanager.backend.readers.TxtReader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 /**
  *
  * @author brigidoalvarado
@@ -12,5 +20,13 @@ public class CardManager {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        TxtReader reader = new TxtReader();
+        try {
+            reader.readTxt("/home/brigidoalvarado/Documentos/prueba.txt");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());;
+        }
     }
+
 }
+
--- CardManager/src/main/java/com/ipc2ss/cardmanager/backend/readers/TxtReader.java
+++ CardManager/src/main/java/com/ipc2ss/cardmanager/backend/readers/TxtReader.java
@@ -12,29 +12,89 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.ipc2ss.cardmanager.enums.TextTypes.*;
 
 /**
  *
  * @author brigidoalvarado
  */
 public class TxtReader {
-    
-    private String txtPath;
-    private String option;
-    
-    public List <CardData> readTxt() throws FileNotFoundException, IOException{
-        
-        File txtFile = new File (txtPath);
-        
-        try (FileReader filereader = new FileReader(txtFile);
-                BufferedReader bufferedReader = new BufferedReader( filereader);
-                ){
-                    while ((option = bufferedReader.readLine()) != null){
-                        
-                        
+
+    private static final String WORD = "\\w+";
+    private static final String START_PRT = "\\(";
+    private static final String END_PRT = "\\)";
+    private static final String CONTENT = ".*";
+    private static final String END_FORMAT = ";";
+    private static final Pattern pattern = Pattern.compile("^"+WORD+START_PRT+CONTENT+END_PRT+END_FORMAT+"$");
+
+    private String keyWord;
+    private String complement;
+
+    public void readTxt(String txtPath) throws FileNotFoundException, IOException{
+
+        try (BufferedReader bufferedReader = new BufferedReader( new FileReader(txtPath));){
+
+            String readText;
+            while ( (readText = bufferedReader.readLine()) != null){
+                validateFormat(readText);
+            }
+
+            try {
+                TextTypes type = TextTypes.valueOf(keyWord);
+                readOption(type);
+            } catch (IllegalArgumentException e){
+                System.out.println("error en el formato");
+            }
+
+
             }
         }
-        return null;
+
+    private void validateFormat(String readText){
+
+        Matcher matcher = pattern.matcher(readText);
+        if (matcher.matches()){
+            String[] words = readText.split(START_PRT,2);
+            if (words.length == 2){
+                keyWord = words[0];
+                String part2 = words[1];
+                complement = part2.substring(0,part2.length()-2);
+
+            } else{
+                System.out.println("lanzar error");
+            }
+        }
+        else {
+            System.out.println("lanzar error");
+        }
+
     }
-        
+
+    private void readOption(TextTypes type){
+
+        switch (type) {
+            case TextTypes.SOLICITUD: System.out.println(SOLICITUD);
+                break;
+            case MOVIMIENTO: System.out.println(MOVIMIENTO);
+                break;
+            case ESTADO_CUENTA: System.out.println(ESTADO_CUENTA);
+                break;
+            case CONSULTAR_TARJETA: System.out.println(CONSULTAR_TARJETA);
+                break;
+            case CANCELACION_TARJETA: System.out.println(CANCELACION_TARJETA);
+                break;
+            case LISTADO_SOLICITUDES: System.out.println(LISTADO_SOLICITUDES);
+                break;
+            case AUTORIZACION_TARJETA: System.out.println(AUTORIZACION_TARJETA);
+                break;
+        }
+    }
+
 }
+
+
+
+
